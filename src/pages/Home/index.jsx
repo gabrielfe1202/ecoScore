0f9faeaab96f03f7d240727e0dbf5bcf0970e524
Felipe Xavier <fellipexavier17@gmail.com>
2024-11-21 18:45:21 -0300

    Corrigindo o nome

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -55,13 +55,13 @@ function Home() {
           <img src={imageEco} className='w-full h-auto rounded-2xl shadow-custom' />
         </div>
         <div className='w-10/12 md:w-6/12 md:pl-16'>
-          <h1 className='text-4xl text-white font-mono'>Como Funciona a EcoStore?</h1>
+          <h1 className='text-4xl text-white font-mono'>Como Funciona a Ecoscore?</h1>
           <p className='text-lg text-white pt-8'>
-            A Ecostore é uma plataforma inovadora que transforma a energia limpa gerada em sua casa em uma moeda poderosa: pontos que podem ser convertidos em uma série de benefícios exclusivos. O funcionamento é simples e intuitivo, permitindo que você aproveite ao máximo a energia renovável que produz. Através de tecnologia de ponta, nossa plataforma realiza um monitoramento detalhado do seu consumo e da geração de energia sustentável, recompensando você a cada quilowatt-hora (kWh) de energia limpa gerada.
+            A Ecoscore é uma plataforma inovadora que transforma a energia limpa gerada em sua casa em uma moeda poderosa: pontos que podem ser convertidos em uma série de benefícios exclusivos. O funcionamento é simples e intuitivo, permitindo que você aproveite ao máximo a energia renovável que produz. Através de tecnologia de ponta, nossa plataforma realiza um monitoramento detalhado do seu consumo e da geração de energia sustentável, recompensando você a cada quilowatt-hora (kWh) de energia limpa gerada.
             <br /><br />
-            Além de ajudar o meio ambiente, a Ecostore oferece uma maneira prática de você obter vantagens reais com o uso de energia solar ou outras fontes renováveis. A cada vez que sua residência ou empresa gera energia limpa, você acumula pontos. Esses pontos podem ser convertidos em descontos em uma ampla gama de produtos e serviços sustentáveis, como itens ecológicos para o lar, produtos orgânicos, soluções de transporte sustentável e muito mais.
+            Além de ajudar o meio ambiente, a Ecoscore oferece uma maneira prática de você obter vantagens reais com o uso de energia solar ou outras fontes renováveis. A cada vez que sua residência ou empresa gera energia limpa, você acumula pontos. Esses pontos podem ser convertidos em descontos em uma ampla gama de produtos e serviços sustentáveis, como itens ecológicos para o lar, produtos orgânicos, soluções de transporte sustentável e muito mais.
             <br /><br />
-            Nossa missão é tornar a sustentabilidade mais acessível e atraente, proporcionando benefícios tangíveis para quem adota práticas que ajudam o planeta. Com a Ecostore, você tem a oportunidade de economizar dinheiro enquanto investe em um futuro mais verde e consciente.
+            Nossa missão é tornar a sustentabilidade mais acessível e atraente, proporcionando benefícios tangíveis para quem adota práticas que ajudam o planeta. Com a Ecoscore, você tem a oportunidade de economizar dinheiro enquanto investe em um futuro mais verde e consciente.
           </p>
         </div>
       </section>
